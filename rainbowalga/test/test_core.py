b98e6a3bcdc420dbfd25d8430ae0b9d9ec10460c
tamasgal <himself@tamasgal.com>
2014-09-03 23:05:04 +0200

    First set fo unit tests

---
+++ rainbowalga/test/test_core.py
@@ -0,0 +1,16 @@
+import unittest
+
+from rainbowalga.core import Position
+
+
+class TestPosition(unittest.TestCase):
+
+    def test_x_y_z(self):
+        position = Position(1, 2, 3)
+        self.assertEqual(1, position.x)
+        self.assertEqual(2, position.y)
+        self.assertEqual(3, position.z)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ rainbowalga/test/test_tools.py
@@ -0,0 +1,74 @@
+import unittest
+from time import sleep
+
+from rainbowalga.tools import Clock
+
+
+class TestClock(unittest.TestCase):
+
+    def test_init(self):
+        clock = Clock()
+
+    def test_time(self):
+        clock = Clock()
+        sleep(0.1)
+        self.assertAlmostEqual(0.1, clock.time, 2)
+
+    def test_speed(self):
+        clock = Clock(speed=2)
+        sleep(0.1)
+        self.assertAlmostEqual(0.2, clock.time, 2)
+
+    def test_reset(self):
+        clock = Clock()
+        sleep(0.1)
+        clock.reset()
+        self.assertAlmostEqual(0, clock.time, 2)
+
+    def test_pause_is_paused(self):
+        clock = Clock()
+        clock.pause()
+        self.assertTrue(clock.is_paused)
+
+    def test_resume_resumes(self):
+        clock = Clock()
+        clock.pause()
+        clock.resume()
+        self.assertFalse(clock.is_paused)
+
+    def test_clock_returns_correct_time_after_pause(self):
+        clock = Clock()
+        sleep(0.1)
+        clock.pause()
+        sleep(0.1)
+        clock.resume()
+        sleep(0.1)
+        self.assertAlmostEqual(0.2, clock.time, 2)
+
+    def test_snooze(self):
+        clock = Clock(snooze_interval=0.2)
+        clock.snooze()
+        sleep(0.1)
+        self.assertTrue(clock.is_snoozed)
+        sleep(0.11)
+        self.assertFalse(clock.is_snoozed)
+
+    def test_fps(self):
+        clock = Clock()
+        for frame in xrange(5):
+            clock.record_frame_time()
+            sleep(0.1)
+        self.assertAlmostEqual(10, clock.fps, 0)
+
+    def test_fps_returns_zero_if_no_frame_recorded(self):
+        clock = Clock()
+        self.assertEqual(0, clock.fps)
+
+    def test_fps_returns_zero_if_only_one_frame_recorded(self):
+        clock = Clock()
+        clock.record_frame_time
+        self.assertEqual(0, clock.fps)
+
+
+if __name__ == '__main__':
+    unittest.main()
