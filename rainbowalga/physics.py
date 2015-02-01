690941484ad986611e8abd89eb0d7ccc2c7c2a0b
Tamas Gal <himself@tamasgal.com>
2015-02-01 14:11:05 +0100

    Refactors calculations

--- rainbowalga/physics.py
+++ rainbowalga/physics.py
@@ -2,6 +2,8 @@ from __future__ import division, absolute_import, print_function
 
 import numpy as np
 
+from km3pipe.dataclasses import Position, Direction
+
 from OpenGL.GL import (glPushMatrix,glLineWidth, glColor3f, glBegin, GL_LINES,
                        glEnd, glVertex3f, glPushMatrix, glPopMatrix,
                        glTranslated)
@@ -9,31 +11,33 @@ from OpenGL.GLUT import glutSolidSphere
 
 
 class Particle(object):
-    def __init__(self, x, y, z, dx, dy, dz, time, speed):
+    def __init__(self, x, y, z, dx, dy, dz, time, speed, length=None):
         self.x = x
         self.y = y
         self.z = z
         self.dx = dx
         self.dy = dy
         self.dz = dz
+        self.pos = Position((x, y, z))
+        self.dir = Direction((dx, dy, dz))
         self.time = time * 1e-9
         self.speed = speed
+        self.length = None
 
-    def draw(self, time, line_width=3, color=(1.0, 0.0, 0.6)):
+    def draw(self, time, line_width=2, color=(1.0, 0.0, 0.6)):
         time = time * 1e-9
+
+        pos_start = self.pos + (self.speed * (-self.time) * self.dir)
+        pos_end = self.pos + (self.speed * (time - self.time) * self.dir)
+
         glPushMatrix()
         glLineWidth(line_width)
         glColor3f(*color)
         glBegin(GL_LINES)
-        glVertex3f(self.x + self.speed * -self.time * self.dx,
-                   self.y + self.speed * -self.time * self.dy,
-                   self.z + self.speed * -self.time * self.dz)
-        glVertex3f(self.x + self.speed * (time - self.time) * self.dx,
-                   self.y + self.speed * (time - self.time) * self.dy,
-                   self.z + self.speed * (time - self.time) * self.dz)
+        glVertex3f(*pos_start)
+        glVertex3f(*pos_end)
         glEnd()
         glPopMatrix()
-
 
 class Hit(object):
     def __init__(self, x, y, z, time, charge=1):
