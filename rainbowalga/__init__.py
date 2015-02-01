52c30406d82742bd66f842b64a67950e2ff93ed0
Tamas Gal <himself@tamasgal.com>
2015-02-01 17:41:04 +0100

    Adds versioning

--- rainbowalga/__init__.py
+++ rainbowalga/__init__.py
@@ -1,1 +1,19 @@
+# coding=utf-8
+# Filename: __init__.py
+"""
+A 3D event display for KM3NeT.
+
+"""
 from __future__ import division, absolute_import, print_function
+
+from rainbowalga.__version__ import version, version_info
+
+__author__ = "Tamas Gal"
+__copyright__ = ("Copyright 2015, Tamas Gal and the KM3NeT collaboration "
+                 "(http://km3net.org)")
+__credits__ = []
+__license__ = "MIT"
+__version__ = version
+__maintainer__ = "Tamas Gal"
+__email__ = "tgal@km3net.de"
+__status__ = "Development"
