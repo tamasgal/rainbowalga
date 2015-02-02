1d4270ec65f24d409b5ebce7edfea94e7a7f3703
Tamas Gal <himself@tamasgal.com>
2015-02-02 11:42:50 +0100

    Changes version and requirements

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(name='rainbowalga',
       install_requires=[
           'numpy',
           'docopt',
-          'km3pipe>=0.5.0',
+          'km3pipe>=0.5.1',
           'Pillow',
           'PyOpenGL',
           'freetype-py',
