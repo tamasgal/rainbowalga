de150c52f8b900ff36a3e46c11726a7c01f48614
Tamas Gal <himself@tamasgal.com>
2015-02-05 00:52:06 +0100

    Changes version to 0.4.7

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(name='rainbowalga',
       install_requires=[
           'numpy',
           'docopt',
-          'km3pipe>=0.5.2',
+          'km3pipe>=0.5.3',
           'Pillow',
           'PyOpenGL',
           'freetype-py',
