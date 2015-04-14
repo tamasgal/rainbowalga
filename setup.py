9f40feac8037b4ddb2a727540bb925183c17eca5
Tamas Gal <himself@tamasgal.com>
2015-04-14 15:24:03 +0200

    Changes version to 0.6.0 and updates dependencies

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(name='rainbowalga',
       install_requires=[
           'numpy',
           'docopt',
-          'km3pipe>=0.6.9',
+          'km3pipe>=0.7.0',
           'Pillow',
           'PyOpenGL',
           'freetype-py',
