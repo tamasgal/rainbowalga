57053c15a2ed2f771e46971553d98bb6aeeacdcf
Tamas Gal <himself@tamasgal.com>
2015-11-11 14:28:39 +0100

    Implements basic Alga

--- rainbowalga/core.py
+++ rainbowalga/core.py
@@ -6,5 +6,19 @@ class Position(object):
         self.y = y
         self.z = z
 
+
 class Alga(object):
-    pass
+    def setup(self, event):
+        pass
+
+    def draw(self, time):
+        """Draw objects within a 3D context"""
+        pass
+
+    def draw_shaded(self, time):
+        """Draw objects within a shaded 3D context"""
+        pass
+
+    def draw2d(self, time):
+        """Draw objects within a 2D context"""
+        pass
